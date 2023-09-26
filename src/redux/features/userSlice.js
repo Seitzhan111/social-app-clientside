e3e15d6e4574b9a9eddcc9b975ccde5eea39d4fa
Seitzhan111 <sbukebaev@gmail.com>
2023-09-26 20:46:17 +0600

    init: rtk configuratin

--- src/redux/features/userSlice.js
+++ src/redux/features/userSlice.js
@@ -4,6 +4,10 @@ import {createSlice} from "@reduxjs/toolkit";
 const userSlice = createSlice({
     name: 'user',
     initialState: {
+        user: null,
+        status: 'idle',
+    },
+    reducers: {},
+});
 
-    }
-})
+export default userSlice.reducer;
