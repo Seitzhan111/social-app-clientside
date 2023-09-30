051ff8ca0215363454cf2f213fde34421e50851d
Seitzhan111 <sbukebaev@gmail.com>
2023-09-30 22:22:05 +0600

    add: header

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
-// import styles from './Footer.module.css'
+import styles from './Footer.module.css'
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer className={styles['footer']}>
+            <div className="container">
+                <div className={styles['wrapper']}>
+                    <p className={styles['copyright']}>
+                        <span>VK</span>
+                        © 2006–2020
+                    </p>
+                    <ul className={styles['links']}>
+                        <li>
+                            <a href="/">About VK</a>
+                        </li>
+                        <li>
+                            <a href="/">Terms</a>
+                        </li>
+                        <li>
+                            <a href="/">Developers</a>
+                        </li>
+                    </ul>
+                    <ul className={styles['languages']}>
+                        <li>
+                            <a href="/">English</a>
+                        </li>
+                        <li>
+                            <a href="/">Русский</a>
+                        </li>
+                        <li>
+                            <a href="/">Українська</a>
+                        </li>
+                        <li>
+                            <a href="/">all languages »</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
     );
 };
 
--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
-// import styles from './Header.module.css'
+import { ReactComponent as VKLogo } from "../../../assets/vectors/content-logo.svg";
+import {InputField} from "../../ui";
+import { IoIosArrowDown } from 'react-icons/io'
+import {HiSearch} from "react-icons/hi";
+import {BiSolidBell} from 'react-icons/bi'
+import styles from './Header.module.css'
+
+
 
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <header className={styles['header']}>
+            <div className="container">
+                <div className={styles['wrapper']}>
+                    <a className={styles['logo']} href="/">
+                        <h1><VKLogo /></h1>
+                    </a>
+                    <div className={styles['handlers']}>
+                        <label className={styles['search']}>
+                            <HiSearch color="#8FADC8" />
+                            <InputField type="text" label="Поиск" />
+                        </label>
+                        <BiSolidBell size='20' color='#1E3C5F' cursor='pointer' />
+                    </div>
+                    <div className={styles['profile']}>
+                        <h3 className={styles['name']}>Elvis</h3>
+                        <div className={styles['avatar']}>
+                            <img src={'https://www.perfocal.com/blog/content/images/2021/01/Perfocal_17-11-2019_TYWFAQ_100_standard-3.jpg'} alt="Картинка профиля" />
+                            <IoIosArrowDown />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
     );
 };
 
